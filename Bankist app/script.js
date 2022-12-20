d45efa3c1c73f9628b2be35823a4444b24c2cd6d
Costan Daniel Solomon <danielcostan99@gmail.com>
2022-12-20 12:24:43 +0200

    Display Summary added

--- Bankist app/script.js
+++ Bankist app/script.js
@@ -72,7 +72,7 @@ const displayMovements = function (movements) {
           <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-          <div class="movements__value">${mov}</div>
+          <div class="movements__value">${mov}€</div>
         </div>
     
     `;
@@ -84,9 +84,31 @@ displayMovements(account1.movements);
 
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
+  labelBalance.textContent = `${balance} €`;
+};
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter((mov) => mov < 0)
+    .reduce((acc, mov) => mov + acc, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * 1.2) / 100)
+    .filter((interest, i, arr) => {
+      console.log(arr);
+      return interest >= 1;
+    })
+    .reduce((acc, interest) => interest + acc, 0);
+  labelSumInterest.textContent = `${interest}€`;
 };
 
+calcDisplaySummary(account1.movements);
 const createUsername = function (acc) {
   acc.forEach(function (acc) {
     acc.username = acc.owner
