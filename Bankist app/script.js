ec2ee780263592b4e7ba89881c9ec61e2c8af2b5
Costan Daniel Solomon <danielcostan99@gmail.com>
2022-12-20 13:08:26 +0200

    Implemented log in based on username and pin

--- Bankist app/script.js
+++ Bankist app/script.js
@@ -80,35 +80,31 @@ const displayMovements = function (movements) {
   });
 };
 
-displayMovements(account1.movements);
-
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
   labelBalance.textContent = `${balance} €`;
 };
-const calcDisplaySummary = function (movements) {
-  const incomes = movements
+const calcDisplaySummary = function (account) {
+  const incomes = account.movements
     .filter((mov) => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${incomes}€`;
 
-  const out = movements
+  const out = account.movements
     .filter((mov) => mov < 0)
     .reduce((acc, mov) => mov + acc, 0);
   labelSumOut.textContent = `${Math.abs(out)}€`;
 
-  const interest = movements
+  const interest = account.movements
     .filter((mov) => mov > 0)
-    .map((deposit) => (deposit * 1.2) / 100)
+    .map((deposit) => (deposit * account.interestRate) / 100)
     .filter((interest, i, arr) => {
-      console.log(arr);
       return interest >= 1;
     })
-    .reduce((acc, interest) => interest + acc, 0);
+    .reduce((acc, interest) => interest + account.interestRate, 0);
   labelSumInterest.textContent = `${interest}€`;
 };
 
-calcDisplaySummary(account1.movements);
 const createUsername = function (acc) {
   acc.forEach(function (acc) {
     acc.username = acc.owner
@@ -119,4 +115,36 @@ const createUsername = function (acc) {
   });
 };
 createUsername(accounts);
-calcDisplayBalance(account1.movements);
+
+//EVENTS HANDLERS
+let currentAccount;
+
+btnLogin.addEventListener("click", function (e) {
+  //Prevent form from submitting
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    (acc) => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    //Display UI and Welcome Message.
+
+    labelWelcome.textContent = `Welcome Back,${
+      currentAccount.owner.split(" ")[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    //Clear input fields
+
+    inputLoginUsername.value = inputLoginPin.value = "";
+    inputLoginPin.blur();
+    //Display Movements.
+    displayMovements(currentAccount.movements);
+    //Display balance.
+    calcDisplayBalance(currentAccount.movements);
+    //Display Summary.
+    calcDisplaySummary(currentAccount);
+  }
+});
